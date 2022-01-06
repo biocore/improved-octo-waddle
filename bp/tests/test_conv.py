a116121daa5f19a98d45e898a713e531b442ca88
Daniel McDonald <danielmcdonald@ucsd.edu>
2022-01-06 08:00:24 -0800

    isolate

--- bp/tests/test_conv.py
+++ bp/tests/test_conv.py
@@ -17,9 +17,6 @@ class ConversionTests(TestCase):
         self.sktn = skbio.TreeNode.read(StringIO(self.tstr))
         print("SETUP", flush=True)
 
-    #def test_blah(self):
-    #    to_skbio_treenode(self.bp)
-
     def test_to_skbio_treenode(self):
         print("tst", flush=True)
         obs = to_skbio_treenode(self.bp)
@@ -33,44 +30,44 @@ class ConversionTests(TestCase):
         self.assertEqual(obs.ascii_art(), self.sktn.ascii_art())
         print("tst", flush=True)
 
- #   def test_from_skbio_treenode(self):
- #       print("fst", flush=True)
- #       obs_bp = from_skbio_treenode(self.sktn)
- #       exp_bp = self.bp
+    def test_from_skbio_treenode(self):
+        print("fst", flush=True)
+        obs_bp = from_skbio_treenode(self.sktn)
+        exp_bp = self.bp
 
- #       npt.assert_equal(obs_bp.B, exp_bp.B)
- #       for i in range(len(self.bp.B)):
- #           self.assertEqual(exp_bp.name(i), obs_bp.name(i))
- #           self.assertEqual(exp_bp.length(i), obs_bp.length(i))
- #       print("fst", flush=True)
+        npt.assert_equal(obs_bp.B, exp_bp.B)
+        for i in range(len(self.bp.B)):
+            self.assertEqual(exp_bp.name(i), obs_bp.name(i))
+            self.assertEqual(exp_bp.length(i), obs_bp.length(i))
+        print("fst", flush=True)
 
-    #def test_to_array(self):
-    #    print("ta", flush=True)
-    #    t = parse_newick('(((a:1,b:2,c:3)x:4,(d:5)y:6)z:7,(e:8,f:9)z:10);')
+    def test_to_array(self):
+        print("ta", flush=True)
+        t = parse_newick('(((a:1,b:2,c:3)x:4,(d:5)y:6)z:7,(e:8,f:9)z:10);')
 
-    #    exp_child_index = np.array([[4, 0, 2], [5, 3, 3], [8, 4, 5], [9, 6, 7],
-    #                                [10, 8, 9]], dtype=np.uint32)
-    #    exp_length = np.array([1, 2, 3, 5, 4, 6, 8, 9, 7, 10, 0.0],
-    #                          dtype=np.double)
-    #    exp_id_index = {0: True, 1: True, 2: True, 3: True, 4: False, 5: False,
-    #                    6: True, 7: True, 8: False, 9: False, 10: False}
-    #    exp_name = np.array(['a', 'b', 'c', 'd', 'x', 'y', 'e', 'f', 'z', 'z',
-    #                        None])
-    #    obs = to_skbio_treearray(t)
+        exp_child_index = np.array([[4, 0, 2], [5, 3, 3], [8, 4, 5], [9, 6, 7],
+                                    [10, 8, 9]], dtype=np.uint32)
+        exp_length = np.array([1, 2, 3, 5, 4, 6, 8, 9, 7, 10, 0.0],
+                              dtype=np.double)
+        exp_id_index = {0: True, 1: True, 2: True, 3: True, 4: False, 5: False,
+                        6: True, 7: True, 8: False, 9: False, 10: False}
+        exp_name = np.array(['a', 'b', 'c', 'd', 'x', 'y', 'e', 'f', 'z', 'z',
+                            None])
+        obs = to_skbio_treearray(t)
 
-    #    obs_child_index = obs['child_index']
-    #    obs_length = obs['length']
-    #    obs_id_index = obs['id_index']
-    #    obs_name = obs['name']
+        obs_child_index = obs['child_index']
+        obs_length = obs['length']
+        obs_id_index = obs['id_index']
+        obs_name = obs['name']
 
-    #    npt.assert_equal(obs_child_index, exp_child_index)
-    #    npt.assert_equal(obs_length, exp_length)
-    #    self.assertEqual(obs_id_index.keys(), exp_id_index.keys())
-    #    npt.assert_equal(obs_name, exp_name)
+        npt.assert_equal(obs_child_index, exp_child_index)
+        npt.assert_equal(obs_length, exp_length)
+        self.assertEqual(obs_id_index.keys(), exp_id_index.keys())
+        npt.assert_equal(obs_name, exp_name)
 
-    #    for k in obs_id_index:
-    #        self.assertEqual(obs_id_index[k].is_tip(), exp_id_index[k])
-    #    print("ta", flush=True)
+        for k in obs_id_index:
+            self.assertEqual(obs_id_index[k].is_tip(), exp_id_index[k])
+        print("ta", flush=True)
 
 if __name__ == '__main__':
     main()
