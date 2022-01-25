49860c08f33fcb994c4e7d6dd063b331c87b0c6d
Daniel McDonald <danielmcdonald@ucsd.edu>
2022-01-25 11:36:43 -0800

    handle underscores

--- bp/_io.pyx
+++ bp/_io.pyx
@@ -115,7 +115,7 @@ def write_newick(BP tree, object output):
 
             if name is not None:
                 # if we have magical characters, make sure we quote
-                if set(name) & {';', ',', '(', ')', ':'}:
+                if set(name) & {';', ',', '(', ')', ':', '_'}:
                     output.write("'%s'" % name)
                 else:
                     output.write(name)
